1d3133347a7d04ba30963b2da537b146a09fab25
uint256_t <konndennsa@gmail.com>
2017-10-05 16:13:29 +0900

    add option parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 extern crate rcaml;
-
 use rcaml::parser;
 
+extern crate clap;
+use clap::{Arg, App};
+
+const VERSION_STR: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    parser::parse_simple_expr("5 /2 + 11 * 10");
-    parser::parse_simple_expr("5.2 /. 0.3");
+    let app = App::new("rcaml")
+        .version(VERSION_STR)
+        .author("uint256_t")
+        .about("rcaml is an OCaml-like implementation in Rust")
+        .arg(Arg::with_name("version").short("v").long("version").help(
+            "Show version info",
+        ))
+        .arg(Arg::with_name("FILE")
+                .help("Input file")
+                // .required(true)
+                .index(1))
+        .get_matches();
+
+    if app.is_present("version") {
+        println!("rcaml {}", VERSION_STR);
+        return;
+    } else if let Some(filename) = app.value_of("FILE") {
+        println!("input filename: {}", filename);
+    } else {
+        parser::parse_simple_expr("5 /2 + 11 * 10");
+        parser::parse_simple_expr("5.2 /. 0.3");
+    }
 }
